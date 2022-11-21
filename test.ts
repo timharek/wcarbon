8c600f7a77680377f4bba86d0edfe870c8475a98
Tim Hårek Andreassen <tim@harek.no>
2022-11-21 23:03:36 +0100

    feat: Add color output
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 import {
   assertEquals,
   assertFalse,
-} from 'https://deno.land/std@0.147.0/testing/asserts.ts';
+} from 'https://deno.land/std@0.165.0/testing/asserts.ts';
 import {
   calculateEnergy,
   calculateSize,
