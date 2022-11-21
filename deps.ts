ad624389d8317a4e676c0f0d5b76380351d6b8be
Tim Hårek Andreassen <tim@harek.no>
2022-11-21 22:56:01 +0100

    chore(deps): Update
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- deps.ts
+++ deps.ts
@@ -1,5 +1,3 @@
-export { parse } from 'https://deno.land/std@0.145.0/flags/mod.ts';
-export type { Args } from 'https://deno.land/std@0.145.0/flags/mod.ts';
-export { printHelp } from 'https://deno.land/x/deno_cli_help@1.0.0/mod.ts';
-export type { Options } from 'https://deno.land/x/deno_cli_help@1.0.0/mod.ts';
-export { Command } from 'https://deno.land/x/cliffy@v0.24.2/command/mod.ts';
+export { parse } from 'https://deno.land/std@0.165.0/flags/mod.ts';
+export type { Args } from 'https://deno.land/std@0.165.0/flags/mod.ts';
+export { Command } from 'https://deno.land/x/cliffy@v0.25.4/command/mod.ts';
