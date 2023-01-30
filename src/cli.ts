f7fa0766f9fb647cead593f3b18f5ceb7691b539
Tim Hårek Andreassen <tim@harek.no>
2023-01-30 19:09:08 +0100

    refactor: Split cli commands
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- src/cli.ts
+++ src/cli.ts
@@ -1,39 +1,21 @@
 // @deno-types='../mod.d.ts'
 
-import { Command, GithubProvider, UpgradeCommand } from '../deps.ts';
+import { Command } from '../deps.ts';
 import { queryData, querySite } from './wcarbon.ts';
 
-await new Command()
-  .name('wcarbon')
-  .version('1.3.2')
-  .description('Query webpages (URLs) via Website Carbons API.')
-  .meta('Author', 'Tim Hårek Andreassen <tim@harek.no>')
-  .meta('Source', 'https://github.com/timharek/wcarbon')
-  .example(
-    'Check site',
-    `wcarbon check timharek.no`,
-  )
-  .example(
-    'Check site but verbose',
-    `wcarbon check timharek.no -v`,
-  )
-  .globalOption('-v, --verbose', 'A more verbose output.', {
-    collect: true,
-    value: (val: boolean, previous = 0) => val ? previous + 1 : 0,
-  })
-  .arguments('<url>')
+const siteCmd = new Command()
+  .description('Calculate the carbon emissions generated per page view.')
+  .example('Check specific site', 'wcarbon site timharek.no')
+  .arguments('<url:string>')
   .action(async (options: { verbose: number }, url: string) =>
     console.log(await querySite(url, options.verbose ?? 0))
-  )
-  .command('site', 'Calculate the carbon emissions generated per page view.')
-  .arguments('<url>')
-  .action(async (options: { verbose: number }, url: string) =>
-    console.log(await querySite(url, options.verbose ?? 0))
-  )
-  .command(
-    'data',
+  );
+
+const dateCmd = new Command()
+  .description(
     'Calculate the emissions of a page by manually passing the bytes and whether or not it is powered by green hosting.',
   )
+  .example('Check emission of page with bytes', 'wcarbon data 1024')
   .arguments('<bytes:number>')
   .option('-g, --green [value:boolean]', 'Whether a page is green or not.', {
     default: false,
@@ -49,13 +31,26 @@ await new Command()
       };
       console.log(await queryData(request, options.verbose));
     },
+  );
+
+await new Command()
+  .name('wcarbon')
+  .version('1.4.0')
+  .description('Query webpages (URLs) via Website Carbons API.')
+  .meta('Author', 'Tim Hårek Andreassen <tim@harek.no>')
+  .meta('Source', 'https://github.com/timharek/wcarbon')
+  .example(
+    'Check site',
+    `wcarbon timharek.no`,
   )
-  .command(
-    'upgrade',
-    new UpgradeCommand({
-      main: 'mod.ts',
-      args: ['--allow-net'],
-      provider: [new GithubProvider({ repository: 'timharek/wcarbon' })],
-    }),
+  .globalOption('-v, --verbose', 'A more verbose output.', {
+    collect: true,
+    value: (val: boolean, previous = 0) => val ? previous + 1 : 0,
+  })
+  .arguments('<url:string>')
+  .action(async (options: { verbose: number }, url: string) =>
+    console.log(await querySite(url, options.verbose ?? 0))
   )
+  .command('site', siteCmd)
+  .command('data', dateCmd)
   .parse(Deno.args);
