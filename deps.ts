c2e6a678694995afeaa3603d21a15f87b6cc7168
Tim Hårek Andreassen <tim@harek.no>
2024-04-16 17:04:46 +0200

    chore(deps): Use deps from JSR and NPM
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export { Command } from 'https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts';
-export * as Colors from 'https://deno.land/std@0.175.0/fmt/colors.ts';
-export { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts';
+export { Command } from 'jsr:@cliffy/command@1.0.0-rc.4';
+export * as Colors from 'jsr:@std/fmt@0.223.0/colors';
+export { z } from 'npm:zod';
 export {
   assertEquals,
   assertExists,
   assertFalse,
-} from 'https://deno.land/std@0.223.0/assert/mod.ts';
-export { toKebabCase } from 'https://deno.land/std@0.223.0/text/mod.ts';
+} from 'jsr:@std/assert@0.223.0';
+export { toKebabCase } from 'jsr:@std/text@0.223.0';
